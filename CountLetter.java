4e59f14cc4f5881d655536b257af6b1e428eefe8
Sabina Karki <sk@Sabinas-MacBook-Pro.local>
2021-09-07 22:32:30 -0500

    lc

--- CountLetter.java
+++ CountLetter.java
@@ -2,23 +2,40 @@
 public class CountLetter {
 	public static void main(String[] args) {
 		String str = "ababbbcccddaaj";
-		int count = 1;
-		char temp = 1;
-		char val;
+		int count = 1 ;
+		String temp = "";
+		String output= "";
+		
 		
 		
 		char[] array = str.toCharArray();
 		for(int i = 0; i < str.length(); i++) {
-			val =array[i];
-			if (temp == val) {
-				count+=1;
+			
+			
+			
+			String CurrentString = String.valueOf(array[i]);
+			
+			if(temp.equals(CurrentString)) {
+				count++;
+				StringBuffer sb = new StringBuffer(output);
+				sb.deleteCharAt(sb.length() - 1);
+				output = String.valueOf(sb);
+				output +=  String.valueOf(count);	
+				
 			}
-			 
-				//if(str.charAt(i) == str.charAt(i+1))
-					System.out.println());
+			else {
+				count = 1;
+				output += CurrentString + String.valueOf(count);	
+				
+				
+			}
+			temp = CurrentString;
+			
+					
 			
 			
 		}
+		System.out.print(output);
 	}
 
 }
